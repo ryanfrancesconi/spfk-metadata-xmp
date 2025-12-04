03d2c4dfa4afe2fe22d2edcf3c4fefd68ebb1e9e
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-12-04 07:37:49 -0800

    package updates fixes

--- Package.swift
+++ Package.swift
@@ -3,185 +3,145 @@
 
 import PackageDescription
 
-let name: String = "SPFKMetadataXMP" // Swift target
-var localDependencies: [RemoteDependency] { [
-    .init(package: .package(url: "\(githubBase)/spfk-base", from: "0.0.1"),
-          product: .product(name: "SPFKBase", package: "spfk-base")),
-    .init(package: .package(url: "\(githubBase)/spfk-time", from: "0.0.1"),
-          product: .product(name: "SPFKTime", package: "spfk-time")),
-    .init(package: .package(url: "\(githubBase)/spfk-testing", from: "0.0.1"),
-          product: .product(name: "SPFKTesting", package: "spfk-testing")),
-    .init(package: .package(url: "\(githubBase)/spfk-utils", from: "0.0.1"),
-          product: .product(name: "SPFKUtils", package: "spfk-utils")),
-] }
-
-let remoteDependencies: [RemoteDependency] = []
-let resources: [PackageDescription.Resource]? = nil
-let testResources: [PackageDescription.Resource]? = [.process("Resources")]
-
-// C/C++ target, nil if no C target
-let nameC: String? = "\(name)C"
-let dependencyNamesC: [String] = []
-let remoteDependenciesC: [RemoteDependency] = [] // 3rd party
-var cSettings: [PackageDescription.CSetting]? { [
-    .headerSearchPath("include_private")
-] }
-var cxxSettings: [PackageDescription.CXXSetting]? { [
-    // Xcode resolves relative to the target root
-    .headerSearchPath("include_private"),
-    .headerSearchPath("Adobe"),
-    .headerSearchPath("Adobe/client-glue"),
-    .headerSearchPath("Adobe/XMPCommon"),
-    .headerSearchPath("Adobe/XMPCore"),
-    .headerSearchPath("Adobe/XMPCommon/Interfaces"),
-    .headerSearchPath("Adobe/XMPCommon/Utilities"),
-    .headerSearchPath("Adobe/XMPCommon/Interfaces/BaseInterfaces"),
-    .headerSearchPath("Adobe/XMPCore/Interfaces")
-] }
-
-let platforms: [PackageDescription.SupportedPlatform]? = [
-    .macOS(.v12)
-]
-
-// MARK: - the binary targets create additional inclusion below that most packages don't have
-
-// Special case for local binary targets
-let binaryTargetNames = ["XMPCore", "XMPFiles"]
-
-var cTargetBinaryDependencies: [PackageDescription.Target.Dependency] {
-    binaryTargetNames.map { .target(name: $0) }
-}
-
-let binaryTargets: [PackageDescription.Target] =
-    binaryTargetNames.map {
-        PackageDescription.Target.binaryTarget(
-            name: $0,
-            path: "Frameworks/\($0).xcframework"
-        )
-    }
-
-// MARK: - Reusable Code for a dual Swift + C with binary frameworks package --------------------------------------------------
-
-let githubBase = "https://github.com/ryanfrancesconi"
-
-struct RemoteDependency {
-    let package: PackageDescription.Package.Dependency
-    let product: PackageDescription.Target.Dependency
-}
-
-var localDependencyNames: [String] {
-    localDependencies.compactMap {
-        switch $0.product {
-        case let .productItem(name: productName, package: _, moduleAliases: _, condition: _):
-            productName
-        default:
-            nil
-        }
-    }
-}
-
-var swiftTarget: PackageDescription.Target {
-    var targetDependencies: [PackageDescription.Target.Dependency] {
-        let names = localDependencyNames.filter { $0 != "SPFKTesting" }
-
-        var value: [PackageDescription.Target.Dependency] = names.map {
-            .byNameItem(name: "\($0)", condition: nil)
-        }
-
-        if let nameC {
-            value.append(.target(name: nameC))
-        }
-
-        value.append(contentsOf: remoteDependencies.map(\.product))
-
-        return value
-    }
-
-    return .target(
-        name: name,
-        dependencies: targetDependencies,
-        resources: resources
-    )
-}
-
-var testTarget: PackageDescription.Target {
-    var targetDependencies: [PackageDescription.Target.Dependency] {
-        var array: [PackageDescription.Target.Dependency] = [
-            .byNameItem(name: name, condition: nil)
-        ]
-
-        if let nameC {
-            array.append(.byNameItem(name: nameC, condition: nil))
-        }
-
-        if localDependencyNames.contains("SPFKTesting") {
-            array.append(.byNameItem(name: "SPFKTesting", condition: nil))
-        }
-
-        return array
-    }
-
-    let nameTests: String = "\(name)Tests" // Test target
-
-    return .testTarget(
-        name: nameTests,
-        dependencies: targetDependencies,
-        resources: testResources,
-        swiftSettings: [
-            .swiftLanguageMode(.v5),
-            .unsafeFlags(["-strict-concurrency=complete"]),
-        ],
-    )
-}
-
-var cTarget: PackageDescription.Target? {
-    guard let nameC else { return nil }
-
-    var targetDependencies: [PackageDescription.Target.Dependency] {
-        var value: [PackageDescription.Target.Dependency] = dependencyNamesC.map {
-            .byNameItem(name: "\($0)", condition: nil)
-        }
-
-        value.append(contentsOf: remoteDependenciesC.map(\.product))
-
-        return value
-    }
-
-    return .target(
-        name: nameC,
-        dependencies: targetDependencies + cTargetBinaryDependencies,
-        publicHeadersPath: "include",
-        cSettings: cSettings,
-        cxxSettings: cxxSettings
-    )
-}
-
-var targets: [PackageDescription.Target] {
-    [swiftTarget, cTarget, testTarget].compactMap(\.self)
-}
-
-var packageDependencies: [PackageDescription.Package.Dependency] {
-    localDependencies.map(\.package) +
-        remoteDependencies.map(\.package) +
-        remoteDependenciesC.map(\.package)
-}
-
-var products: [PackageDescription.Product] {
-    let targets: [String] = [name, nameC].compactMap(\.self)
-
-    return [
-        .library(name: name, targets: targets)
-    ]
-}
-
-// This is required to be at the bottom
-
 let package = Package(
-    name: name,
+    name: "spfk-metadata-xmp",
     defaultLocalization: "en",
-    platforms: platforms,
-    products: products,
-    dependencies: packageDependencies,
-    targets: targets + binaryTargets,
+    platforms: [
+        .macOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "SPFKMetadataXMP",
+            targets: [
+                "SPFKMetadataXMP",
+                "SPFKMetadataXMPC",
+            ]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ryanfrancesconi/spfk-base", branch: "development"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-time", branch: "development"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-utils", branch: "development"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-testing", branch: "development"),
+    ],
+    targets: [
+        .target(
+            name: "SPFKMetadataXMP",
+            dependencies: [
+                "SPFKMetadataXMPC",
+                .product(name: "SPFKBase", package: "spfk-base"),
+                .product(name: "SPFKTime", package: "spfk-time"),
+                .product(name: "SPFKUtils", package: "spfk-utils"),
+            ]
+        ),
+
+        .target(
+            name: "SPFKMetadataXMPC",
+            dependencies: [
+                .target(name: "XMPCore"),
+                .target(name: "XMPFiles"),
+            ],
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath("include_private")
+            ],
+            cxxSettings: [
+                .headerSearchPath("include_private"),
+                .headerSearchPath("Adobe"),
+                .headerSearchPath("Adobe/client-glue"),
+                .headerSearchPath("Adobe/XMPCommon"),
+                .headerSearchPath("Adobe/XMPCore"),
+                .headerSearchPath("Adobe/XMPCommon/Interfaces"),
+                .headerSearchPath("Adobe/XMPCommon/Utilities"),
+                .headerSearchPath("Adobe/XMPCommon/Interfaces/BaseInterfaces"),
+                .headerSearchPath("Adobe/XMPCore/Interfaces")
+            ]
+        ),
+
+        .binaryTarget(
+            name: "XMPCore",
+            path: "Frameworks/XMPCore.xcframework"
+        ),
+        .binaryTarget(
+            name: "XMPFiles",
+            path: "Frameworks/XMPFiles.xcframework"
+        ),
+
+        .testTarget(
+            name: "SPFKMetadataXMPTests",
+            dependencies: [
+                "SPFKMetadataXMP",
+                "SPFKMetadataXMPC",
+                .product(name: "SPFKTesting", package: "spfk-testing"),
+
+            ],
+            resources: [.process("Resources")],
+            swiftSettings: [
+                .swiftLanguageMode(.v5),
+                .unsafeFlags(["-strict-concurrency=complete"]),
+            ],
+        ),
+    ],
     cxxLanguageStandard: .cxx20
 )
+
+/*
+ let name: String = "SPFKMetadataXMP" // Swift target
+ var localDependencies: [RemoteDependency] { [
+     .init(package: .package(url: "\(githubBase)/spfk-base", from: "0.0.1"),
+           product: .product(name: "SPFKBase", package: "spfk-base")),
+     .init(package: .package(url: "\(githubBase)/spfk-time", from: "0.0.1"),
+           product: .product(name: "SPFKTime", package: "spfk-time")),
+     .init(package: .package(url: "\(githubBase)/spfk-testing", from: "0.0.1"),
+           product: .product(name: "SPFKTesting", package: "spfk-testing")),
+     .init(package: .package(url: "\(githubBase)/spfk-utils", from: "0.0.1"),
+           product: .product(name: "SPFKUtils", package: "spfk-utils")),
+ ] }
+
+ let remoteDependencies: [RemoteDependency] = []
+ let resources: [PackageDescription.Resource]? = nil
+ let testResources: [PackageDescription.Resource]? = [.process("Resources")]
+
+ // C/C++ target, nil if no C target
+ let nameC: String? = "\(name)C"
+ let dependencyNamesC: [String] = []
+ let remoteDependenciesC: [RemoteDependency] = [] // 3rd party
+ var cSettings: [PackageDescription.CSetting]? { [
+     .headerSearchPath("include_private")
+ ] }
+ var cxxSettings: [PackageDescription.CXXSetting]? { [
+     // Xcode resolves relative to the target root
+     .headerSearchPath("include_private"),
+     .headerSearchPath("Adobe"),
+     .headerSearchPath("Adobe/client-glue"),
+     .headerSearchPath("Adobe/XMPCommon"),
+     .headerSearchPath("Adobe/XMPCore"),
+     .headerSearchPath("Adobe/XMPCommon/Interfaces"),
+     .headerSearchPath("Adobe/XMPCommon/Utilities"),
+     .headerSearchPath("Adobe/XMPCommon/Interfaces/BaseInterfaces"),
+     .headerSearchPath("Adobe/XMPCore/Interfaces")
+ ] }
+
+ let platforms: [PackageDescription.SupportedPlatform]? = [
+     .macOS(.v12)
+ ]
+
+ // MARK: - the binary targets create additional inclusion below that most packages don't have
+
+ // Special case for local binary targets
+ let binaryTargetNames = ["XMPCore", "XMPFiles"]
+
+ var cTargetBinaryDependencies: [PackageDescription.Target.Dependency] {
+     binaryTargetNames.map { .target(name: $0) }
+ }
+
+ let binaryTargets: [PackageDescription.Target] =
+     binaryTargetNames.map {
+         PackageDescription.Target.binaryTarget(
+             name: $0,
+             path: "Frameworks/\($0).xcframework"
+         )
+     }
+
+ /
+ */
