7b34de98bca43681e1bf4704ae5b489139f26218
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-03-05 07:43:54 -0800

    up package requirements to current minimums

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/ryanfrancesconi/spfk-base", from: "0.0.3"),
         .package(url: "https://github.com/ryanfrancesconi/spfk-time", from: "0.0.1"),
-        .package(url: "https://github.com/ryanfrancesconi/spfk-utils", from: "0.0.3"),
-        .package(url: "https://github.com/ryanfrancesconi/spfk-testing", from: "0.0.1"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-utils", from: "0.0.8"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-testing", from: "0.0.5"),
     ],
     targets: [
         .target(
